3c315f69806190955972877cdca382bfe17424c2
vickydeveloper <vickyroman404@gmail.com>
2024-01-04 10:02:55 +0530

    added encrytion and decryption method

--- src/main/java/org/example/Header.java
+++ src/main/java/org/example/Header.java
@@ -2,22 +2,36 @@ package org.example;
 
 import org.json.JSONObject;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Base64;
+import java.util.List;
 
 public class Header {
+
+
+    public static final String SECRET_KEY = "igrsdjkkeuhdiwdh";
+
     public static void main(String[] args) {
+
+
         try {
             String firstName = "77";
             String email = "uusd.dass@example.com";
 
-            String encodedFirstName = base64Encode(firstName);
-            String encodedEmail = base64Encode(email);
+            String encodedFirstName = aesEncrypt(firstName);
+            String encodedEmail = aesEncrypt(email);
+
+            String decodedFirstName = aesDecrypt(encodedFirstName);
+            String decodedEmail = aesDecrypt(encodedEmail);
+
 
             String apiURL = "http://localhost:8080/student/studentHeader";
 
@@ -27,8 +41,8 @@ public class Header {
             con.setRequestMethod("POST");
             con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             con.setRequestProperty("Accept", "application/json");
-            con.setRequestProperty("firstName", encodedFirstName);
-            con.setRequestProperty("email", encodedEmail);
+            con.setRequestProperty("firstName", decodedFirstName);
+            con.setRequestProperty("email", decodedEmail);
 
             con.setDoOutput(true);
 
@@ -41,11 +55,15 @@ public class Header {
 
                 JSONObject jsonResponse = new JSONObject(response.toString());
 
-                String decodedFirstName = base64Decode(jsonResponse.getString("firstName"));
-                String decodedEmail = base64Decode(jsonResponse.getString("email"));
+//                String decodedFirstName = base64Decode(jsonResponse.getString("firstName"));
+//                String decodedEmail = base64Decode(jsonResponse.getString("email"));
 
-                jsonResponse.put("firstName",decodedFirstName);
-                jsonResponse.put("email",decodedEmail);
+                String firstNameData = jsonResponse.getString("firstName");
+                String emailData = jsonResponse.getString("email");
+
+                jsonResponse.put("firstName", firstNameData);
+                jsonResponse.put("email", emailData);
+
                 jsonResponse.remove("lastName");
                 jsonResponse.remove("phoneNumber");
                 jsonResponse.remove("course");
@@ -67,15 +85,26 @@ public class Header {
         }
     }
 
-    private static String base64Encode(String input) {
-        byte[] encodedBytes = Base64.getEncoder().encode(input.getBytes(StandardCharsets.UTF_8));
-        return new String(encodedBytes);
+    private static String aesEncrypt(String input) throws Exception {
+        Key key = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(input.getBytes(StandardCharsets.UTF_8));
+        String encryptedString = Base64.getEncoder().encodeToString(encryptedBytes);
+
+        System.out.println("Encrypted data: " + encryptedString);
+        return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    private static String base64Decode(String input) {
-        byte[] decodedBytes = Base64.getDecoder().decode(input);
-        String decodedValue = new String(decodedBytes, StandardCharsets.UTF_8);
+    private static String aesDecrypt(String input) throws Exception {
 
-        return decodedValue;
+        Key key = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(input));
+        String decryptedString = new String(decryptedBytes, StandardCharsets.UTF_8);
+
+        System.out.println("Decrypted data: " + decryptedString);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
     }
 }
